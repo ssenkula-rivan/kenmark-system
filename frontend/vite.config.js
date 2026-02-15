f77ee5a3dc24baedd8a5d41e49820e06f6e717fd
ssenkula-rivan <cranictech@gmail.com>
2026-02-15 12:14:02 +0300

    Fix terser issue - use esbuild minifier instead

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,12 +16,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
+    minify: 'esbuild'
   }
 });
